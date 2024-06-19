44e5380355ed5fa2775929023135ba6be3d91009
RagiliawanPutraa <ragiliawanputraa@gmail.com>
2024-06-19 16:43:56 +0700

    COMMIT PERTAMA KALI

---
+++ javascript/animasi.js
@@ -0,0 +1,15 @@
+var typed = new Typed('.title', { //membuat efek mengetik
+  strings: ["HELLO SWIFTIES!!!"],
+  typeSpeed: 100, //kecepatan pengetikan karakter baru
+  backSpeed: 200, // kecepatan pengetikan kembali karakter
+  loop: false,
+  showCursor: false
+});
+
+var typed2 = new Typed('.subtitle', {
+  strings: ["Taylor Swift Fansite"],
+  typeSpeed: 100,
+  backSpeed: 200,
+  loop: false,
+  showCursor: false
+});
